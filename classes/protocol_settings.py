d4b4b2c8391d63254aa38aef7fe4b0a11b731496
Tmaxxrox97 <tmaxxrox@gmail.com>
2026-03-08 17:31:27 -0500

    Adjuested regex for negative values

--- classes/protocol_settings.py
+++ classes/protocol_settings.py
@@ -400,9 +400,9 @@ class protocol_settings:
         data_type_regex = re.compile(r"(?P<datatype>\w+)\.(?P<length>\d+)")
 
         #f... cant do -100 to -25 for example, due to - as divider. might have to do all ranges with ~?
-        range_regex = re.compile(r"(?P<reverse>r|)(?P<start>(?:0?x[\-\da-z]+|[\d]+))[\-~](?P<end>(?:0?x[\da-z]+|[\d]+))")
+        range_regex = re.compile(r"(?P<reverse>r|)(?P<start>(?:0?x[\da-z]+|[\-\d]+))[\-~](?P<end>(?:0?x[\da-z]+|[\d]+))")
         ascii_value_regex = re.compile(r"(?P<regex>^\[.+\]$)")
-        list_regex = re.compile(r"\s*(?:(?P<range_start>(?:0?x[\-\da-z]+|[\d]+))[\-~](?P<range_end>(?:0?x[\da-z]+|[\d]+))|(?P<element>[^,\s][^,]*?))\s*(?:,|$)")
+        list_regex = re.compile(r"\s*(?:(?P<range_start>(?:0?x[\da-z]+|[\-\d]+))[\-~](?P<range_end>(?:0?x[\da-z]+|[\d]+))|(?P<element>[^,\s][^,]*?))\s*(?:,|$)")
 
 
         #load read_interval from transport settings, for #x per register read intervals
--- defs/common.py
+++ defs/common.py
@@ -42,7 +42,6 @@ def strtoint(val : str) -> int:
         return val
 
     val = val.lower().strip()
-    val = re.sub(r'^\W+|\W+$', '', val)
 
     if val and val[0] == "x":
         val = val[1:]
